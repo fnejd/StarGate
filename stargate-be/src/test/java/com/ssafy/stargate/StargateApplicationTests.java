1452c91549fdbfb240f5da8c3320c1e1c8b85060
inputTitleHere <seanbryan97@naver.com>
2023-07-17 17:42:19 +0900

    master, backend spring boot initializing

---
+++ stargate-be/src/test/java/com/ssafy/stargate/StargateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.stargate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StargateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
