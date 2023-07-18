dba79985aa7b90931efd414d16c0853c4045e241
이의찬 <cksxkr5193@naver.com>
2023-07-18 17:42:29 +0900

    settingtailwind css에 font와 color를 세팅하였습니다.

--- Stargate-Fe/src/App.tsx
+++ Stargate-Fe/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 className="text-3xl font-bold underline">Vite + React</h1>
+      <h1 className="t1b">T1/B Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
