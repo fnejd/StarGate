d524fea26038a49733d870c31d4e16857dff4b8e
남현실 <hsilnam3@gmail.com>
2023-07-19 14:48:16 +0900

    feature: FUser 로그인 기능 추가 [S09P12A406-92]

--- stargate-be/src/main/java/com/ssafy/stargate/model/service/FUserService.java
+++ stargate-be/src/main/java/com/ssafy/stargate/model/service/FUserService.java
@@ -1,11 +1,16 @@
 package com.ssafy.stargate.model.service;
 
+import com.ssafy.stargate.exception.LoginException;
 import com.ssafy.stargate.exception.RegisterException;
+import com.ssafy.stargate.model.dto.JwtResponseDto;
+import com.ssafy.stargate.model.dto.request.FUserLoginRequestDto;
 import com.ssafy.stargate.model.dto.request.FUserRegisterRequestDto;
 
 /**
  * 팬 유저 서비스 인터페이스
+ * @author 남현실
  */
 public interface FUserService {
     public void create(FUserRegisterRequestDto dto) throws RegisterException;
+    public JwtResponseDto login(FUserLoginRequestDto dto) throws LoginException;
 }
