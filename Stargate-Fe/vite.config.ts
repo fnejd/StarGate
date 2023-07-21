f051b1aa68daf1810714109fe5e83f10afc98398
yeryunJung <2bites@naver.com>
2023-07-21 15:38:27 +0900

    style: prettier, vite config 세팅

--- Stargate-Fe/vite.config.ts
+++ Stargate-Fe/vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 const viteConfig = defineConfig({
-  plugins: [react()],
   server: {
-    host: 'localhost',
-    port: 3000,
+      host: 'localhost',
+      port: 3000,
+    },
+  resolve: {
+    alias: [
+      { find: "@", replacement: "/src" },
+    ],
   },
 })
 
