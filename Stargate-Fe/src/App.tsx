b275624d6902d7681a82202df7dc3c36f9daaad9
yeryunJung <2bites@naver.com>
2023-07-21 10:24:09 +0900

    chore: 머지 리퀘스트 템플릿 세팅 [S09P12A406-61]

--- Stargate-Fe/src/App.tsx
+++ Stargate-Fe/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 className="t1b">T1/B Vite + React</h1>
+      <h1 className="t3b">T1/B Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
