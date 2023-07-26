146a9d6f3c52f31f9a35617fd6e5f1c9c06dea0a
yeryunJung <2bites@naver.com>
2023-07-26 17:18:33 +0900

    feature: 리마인드 사인회 정보 섹션 구현

--- Stargate-Fe/src/pages/auth/SignIn.tsx
+++ Stargate-Fe/src/pages/auth/SignIn.tsx
@@ -1,27 +1,29 @@
-import BtnWhite from '@/atoms/BtnWhite.tsx'
-import BtnBlue from '@/atoms/BtnBlue.tsx'
-import BlackLayer from '@/atoms/BlackLayer.tsx'
-import PolariodCard from '@/atoms/PolariodCard.tsx'
+import BtnWhite from '@/atoms/BtnWhite.tsx';
+import BtnBlue from '@/atoms/BtnBlue.tsx';
+import BlackLayer from '@/atoms/BlackLayer.tsx';
+import PolariodCard from '@/atoms/PolariodCard.tsx';
 import AdminBtn from '@/atoms/AdminBtn.tsx';
 import AdminInput from '@/atoms/AdminInput.tsx';
 import MeetingLeftSection from '@/organisms/MeetingLeftSection.tsx';
 import MeetingRightSection from '@/organisms/MeetingRightSection.tsx';
 import MeetingBottomSection from '@/organisms/MeetingBottomSection.tsx';
+import RemindTitle from '@/organisms/RemindTitle.tsx';
 
 const SignIn = () => {
   const handleClick = () => {
-    console.log("Button clicked!");
+    console.log('Button clicked!');
   };
   const imageUrl = 'https://picsum.photos/900/700';
   const caption = '사진 캡션';
-  const label = '라벨'
+  const label = '라벨';
 
   return (
     // 임시
-    <div className='bg-slate-700'>
+    <div className="bg-slate-700">
       {/* <MeetingLeftSection /> */}
       {/* <MeetingRightSection /> */}
-      <MeetingBottomSection />
+      {/* <MeetingBottomSection /> */}
+      <RemindTitle />
       {/* <BtnWhite text="Click me" onClick={handleClick} />
       <BtnBlue text="Click me" onClick={handleClick} />
       <PolariodCard imageUrl={imageUrl} caption={caption} />
