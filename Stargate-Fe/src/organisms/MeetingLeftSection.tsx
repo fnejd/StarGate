4e5f1b3b7aec75208d44ae83d2166712b3a5fe4d
YeryunJung <2bites@naver.com>
2023-07-30 22:40:54 +0900

    feature: 총 미팅 시간 구현 및 80초 미만 알럿 적용

--- Stargate-Fe/src/organisms/MeetingLeftSection.tsx
+++ Stargate-Fe/src/organisms/MeetingLeftSection.tsx
@@ -12,8 +12,8 @@ interface FormData {
 
 const MeetingLeftSection = () => {
   // const [startDate, setStartDate] = useState<Date>(new Date());
-  const [picSec, setPicSec] = useState<string>('40');
-  const [totalSec, setTotalSec] = useState<number>('80');
+  const [picSec, setPicSec] = useState<number>(40);
+  const [totalSec, setTotalSec] = useState<number>(80);
   const [numbers, setNumbers] = useState<number[]>([4]);
   const [formData, setFormData] = useState<FormData>({
     startDate: null,
@@ -43,14 +43,17 @@ const MeetingLeftSection = () => {
     console.log(formData);
   };
 
-  const handleTotalTime = (
-    event: React.ChangeEventHandler<HTMLInputElement>
-  ) => {
-    const newTime: number = parseInt(event.currentTarget.value, 10);
-    setTotalSec(newTime);
-    console.log(`전체 미팅 시간은 ${newTime}초`);
+  const handleTotalTime = (value: number) => {
+    // const newTime: number = value;
 
-    const cnt = Math.floor(newTime / 2 / 10);
+    if (value <= 80) {
+      alert('전체 미팅 시간은 80초보다 커야 합니다.');
+    }
+
+    setTotalSec(value);
+    console.log(`전체 미팅 시간은 ${value}초`);
+
+    const cnt = Math.floor(value / 2 / 10);
     console.log(`컷 수는 ${cnt}`);
 
     let newNumbers: number[] = [4];
@@ -78,12 +81,15 @@ const MeetingLeftSection = () => {
       <div className="flex items-end">
         <AdminInput
           labelFor="총 미팅 시간"
-          type="text"
+          type="number"
           placeholder="80"
-          value={setTotalSec}
+          value={totalSec} // 숫자를 문자열로 변환하여 value에 설정
           onInputChange={handleTotalTime}
-        />
-        <p className="font-suit font-medium text-14 text-white p-1 pl-2">초</p>
+        >
+          <p className="font-suit font-medium text-14 text-white p-1 pl-2">
+            초
+          </p>
+        </AdminInput>
       </div>
 
       <div className="w-48 h-8 leading-8 mx-1 mt-4 font-suit font-semibold text-14 text-white text-left">
@@ -100,10 +106,12 @@ const MeetingLeftSection = () => {
 
       <div className="flex items-end">
         <DropDown numbers={numbers} onOptionChange={handlePicSecChange} />
-        <p className="font-suit font-medium text-14 text-white p-1 pl-2">컷</p>
+        <p className="font-suit font-medium text-14 text-white p-1 pl-2 mb-1">
+          컷
+        </p>
       </div>
       <div className="mx-1 my-2 font-suit font-medium text-14 text-white">
-        한 멤버당 미팅 시간은 사인회 시간 {totalSec - picSec} 초와 촬영 시간{' '}
+        연예인 1 명의 미팅 시간은 사인회 시간 {totalSec - picSec} 초와 촬영 시간{' '}
         {picSec} 초를 더한 {totalSec} 초 입니다
       </div>
     </>
