bda1feaf39cc616934d71f54c3a692845f121b9d
yryn2016 <yryn2016@gmail.com>
2023-08-01 15:48:19 +0900

    chore: 필요없는 주석 제거

--- Stargate-Fe/src/pages/user/video/UserVideo.tsx
+++ Stargate-Fe/src/pages/user/video/UserVideo.tsx
@@ -3,14 +3,11 @@ import ReactPlayer from 'react-player';
 import peerService from '@/service/peer';
 
 const UserVideo = () => {
-  // 16밀리초 단위로 state를 업데이트
   const [myStream, setMyStream] = useState<MediaStream | null>(null);
   const [remoteStream, setRemoteStream] = useState<MediaStream | null>(null);
-  const myVideoRef = useRef<HTMLVideoElement | null>(null);
   const [socket, setSocket] = useState<WebSocket | null>(
     new WebSocket('ws://i9a406.p.ssafy.io:8080/rtc/asdf.12')
   );
-  // const [socket, setSocket] = useState<WebSocket | null>(null);
 
   const sendStreams = useCallback(() => {
     for (const track of myStream.getTracks()) {
@@ -59,33 +56,8 @@ const UserVideo = () => {
     console.log('3. 응답보냄');
   }, []);
 
-  // 웹소켓으로부터 메시지를 받아 처리하는 함수
-  // 이 안에 경우 나눌거임
-  // const handleMessage = useCallback((event: MessageEvent) => {
-  //   console.log('EVENT = ', event); // 받은 메시지의 이벤트 정보를 로그 출력
-  //   // const data = JSON.parse(event.data);
-
-  //   // // 상대가 조인했다는 메시지를 받음
-  //   // if (data.message === 'join') {
-  //   //   handleCallStart();
-  //   // }
-
-  //   // if (data.message === 'offer') {
-  //   //   console.log('오퍼받음');
-  //   //   // data.offer로 필요한 처리를 하세요.
-  //   // }
-  // }, []);
-
   useEffect(() => {
     console.log('컴포넌트 실행');
-
-    // 웹소켓 서버 URL 설정
-    // const socketUrl = 'ws://i9a406.p.ssafy.io:8080/rtc/asdf.12';
-    const socketUrl = 'ws://i9a406.p.ssafy.io:8080/rtc/asdf.12';
-
-    // 웹소켓 객체를 생성하고 서버와 연결합니다.
-    // let socket = new WebSocket(socketUrl);
-    // setSocket(socket);
 
     socket.onopen = async () => {
       console.log('서버 오픈~');
@@ -105,11 +77,6 @@ const UserVideo = () => {
       console.log('조인 보낸다');
       socket.send(dataString); // 서버로 들어왔다는 메시지 'join' 전송
 
-      // 웹소켓으로부터 메시지를 받아 처리하는 이벤트 핸들러를 등록합니다.
-      // socket.onmessage = (event) => {
-      //   console.log('EVENT = ', event); // 받은 메시지의 이벤트 정보를 로그 출력
-      //   handleMessage(event); // handleMessage 함수 실행
-      // };
       socket.onmessage = async (event) => {
         console.log('EVENT = ', event); // 받은 메시지의 이벤트 정보를 로그 출력
         const receivedData = JSON.parse(event.data);
@@ -143,16 +110,6 @@ const UserVideo = () => {
       };
     };
   }, []);
-
-  // useEffect(() => {
-  //   // 웹소켓으로부터 메시지를 받아 처리하는 이벤트 핸들러를 등록합니다.
-  //   socket?.addEventListener('message', handleMessage);
-
-  //   return () => {
-  //     // 컴포넌트 언마운트 시 이벤트 핸들러 제거
-  //     socket?.removeEventListener('message', handleMessage);
-  //   };
-  // }, [socket, handleMessage]);
 
   return (
     <div>
