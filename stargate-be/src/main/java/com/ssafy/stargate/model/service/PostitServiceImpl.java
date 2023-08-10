531ce665d9a732662a57e8c78845098f65914787
inputTitleHere <seanbryan97@naver.com>
2023-08-10 16:48:54 +0900

    be/refactor/postit, 포스트잇 request 형식을 프런트 담당자 요청에 따라 변경

--- stargate-be/src/main/java/com/ssafy/stargate/model/service/PostitServiceImpl.java
+++ stargate-be/src/main/java/com/ssafy/stargate/model/service/PostitServiceImpl.java
@@ -5,9 +5,6 @@ import com.ssafy.stargate.model.dto.request.postit.PostitDeleteRequestDto;
 import com.ssafy.stargate.model.dto.request.postit.PostitRequestDto;
 import com.ssafy.stargate.model.dto.request.postit.PostitWriteRequestDto;
 import com.ssafy.stargate.model.dto.response.postit.PostitResponseDto;
-import com.ssafy.stargate.model.entity.FUser;
-import com.ssafy.stargate.model.entity.Meeting;
-import com.ssafy.stargate.model.entity.PMember;
 import com.ssafy.stargate.model.entity.Postit;
 import com.ssafy.stargate.model.repository.FUserRepository;
 import com.ssafy.stargate.model.repository.MeetingRepository;
@@ -16,6 +13,8 @@ import com.ssafy.stargate.model.repository.PostitRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
@@ -33,28 +32,23 @@ public class PostitServiceImpl implements PostitService {
     /**
      * 포스트잇을 작성한다.
      * 기존의 포스트잇이 있으면 정보를 업데이트한다.
+     *
      * @param postitDto 신규 포스트잇 정보가 담긴 DTO객체
      * @throws CRUDException 포스트잇 작성 실패
      */
     @Override
     public void writePostit(PostitWriteRequestDto postitDto) throws CRUDException {
         try {
-            Postit postit = postitRepository.findPostit(postitDto.getFanEmail(), postitDto.getMemberNo(), postitDto.getMeetingUuid()).orElse(null);
-            if (postit == null) {
-                FUser fUser = fUserRepository.getReferenceById(postitDto.getFanEmail());
-                PMember pMember = pMemberRepository.getReferenceById(postitDto.getMemberNo());
-                Meeting meeting = meetingRepository.getReferenceById(postitDto.getMeetingUuid());
-                postit = Postit.builder()
-                        .fUser(fUser)
-                        .pMember(pMember)
-                        .meeting(meeting)
-                        .contents(postitDto.getContents())
-                        .build();
-                postitRepository.save(postit);
-            } else {
-                postit.setContents(postitDto.getContents());
-                postitRepository.save(postit);
-            }
+            List<Postit> postitList = postitDto.getPostitDatas().stream().map(postitData -> {
+                Postit postit = postitRepository.findPostit(postitDto.getEmail(), postitData.getMemberNo(), postitDto.getUuid()).orElse(Postit.builder()
+                        .fUser(fUserRepository.getReferenceById(postitDto.getEmail()))
+                        .pMember(pMemberRepository.getReferenceById(postitData.getMemberNo()))
+                        .meeting(meetingRepository.getReferenceById(postitDto.getUuid()))
+                        .build());
+                postit.setContents(postitData.getPostitContents());
+                return postit;
+            }).toList();
+            postitRepository.saveAll(postitList);
         } catch (Exception e) {
             log.warn(e.getMessage());
             throw new CRUDException("포스트잇 작성 중 오류가 발생했습니다.");
@@ -63,6 +57,7 @@ public class PostitServiceImpl implements PostitService {
 
     /**
      * 팬 이메일, 멤버 번호, 싸인회 고유번호에 따라 포스트잇을 쿼리해온다.
+     *
      * @param postitDto email(fan), memberNo, uuid(meeting)을 담은 dto
      * @return 쿼리한 포스트잇 dto 객체
      */
@@ -74,6 +69,7 @@ public class PostitServiceImpl implements PostitService {
 
     /**
      * 팬 이메일, 멤버 번호, 싸인회 고유번호를 기준으로 포스트잇을 삭제한다.
+     *
      * @param postitDto : PostitDto : no만 필요.
      */
     @Override
