a0338c62dbb95f1fdbec4a74993d4a02b62fcdfe
YeryunJung <2bites@naver.com>
2023-08-15 15:35:22 +0900

    chore: develop 머지 후 충돌 해결

--- Stargate-Fe/src/organisms/video/VideoHeaderComponent.tsx
+++ Stargate-Fe/src/organisms/video/VideoHeaderComponent.tsx
@@ -56,18 +56,13 @@ const VideoHeaderComponent: React.FC<VideoHeaaderProps> = ({
     <div className="flex flex-row w-screen my-5">
       <div className="basis-1/2 w-full text-white ml-5">
         <ProFileIcon />
-        <div className="mx-auto">
-          <p>이름: user.name</p>
-          <p>생일: user.birthday</p>
-          <p>촬영 여부: user.isPolaroidEnable</p>
-        </div>
       </div>
       <div className="flex basis-1/4 justify-end mr-5">
         <TimeLeftComponent min={min} sec={sec} />
       </div>
       {/* 다음 사람 뜨는 텍스트 부분 */}
-      <div className="flex justify-center text-white basis-1/4">
-        <p className="mt-1 text-xl">NEXT</p>
+      <div className="flex basis-1/4 justify-center text-white">
+        <p className="text-xl mt-1">NEXT</p>
         <p className="modal-title">{nextUser}</p>
       </div>
     </div>
