679c6b969dc07b4391c776eb0922accc71cadf4d
l0u0h0 <l0u0h0@gmail.com>
2023-08-15 17:52:17 +0900

    Fix: 머지 충돌 문제 해결, 쿼리스트링으로 변환

--- Stargate-Fe/src/pages/user/board/Remind.tsx
+++ Stargate-Fe/src/pages/user/board/Remind.tsx
@@ -1,10 +1,3 @@
-import React from 'react';
-
-const Remind = () => {
-  return <div>리마인드</div>;
-};
-
-export default Remind;
 import { useState, useEffect } from 'react';
 import RemindDetail from '@/organisms/remind/RemindDetail';
 import RemindTitle from '@/organisms/remind/RemindTitle';
