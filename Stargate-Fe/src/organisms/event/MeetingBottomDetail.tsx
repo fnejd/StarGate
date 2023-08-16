947a821737bc43298ba922f3f0fcf222b95cef35
Uichan Lee <cksxkr5193@naver.com>
2023-08-16 13:31:39 +0900

    chore : MeetingDetail UI 개선

--- Stargate-Fe/src/organisms/event/MeetingBottomDetail.tsx
+++ Stargate-Fe/src/organisms/event/MeetingBottomDetail.tsx
@@ -3,8 +3,8 @@ import { MeetingData } from '@/types/event/type';
 const MeetingBottomDetail = ({ formData }: { formData: MeetingData }) => {
   console.log('이건가', formData.meetingFUsers);
   return (
-    <div className="w-full h-96 justify-evenly flex flex-col items-start font-medium text-white font-suit text-14">
-      <div className="flex">유저</div>
+    <div className="w-full flex flex-col items-start font-medium text-white font-suit text-14">
+      <div className="flex text-16 font-semibold">유저</div>
       <table>
         <tbody>
           <tr>
