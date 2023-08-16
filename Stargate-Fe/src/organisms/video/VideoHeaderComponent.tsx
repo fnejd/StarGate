737c763a4e3bd120158c7ca0c4d2d5224a7058f6
l0u0h0 <l0u0h0@gmail.com>
2023-08-16 10:10:36 +0900

    Fix: VideoHeader 다음 유저 표시 부분 수정

--- Stargate-Fe/src/organisms/video/VideoHeaderComponent.tsx
+++ Stargate-Fe/src/organisms/video/VideoHeaderComponent.tsx
@@ -29,7 +29,17 @@ const VideoHeaderComponent: React.FC<VideoHeaaderProps> = ({
   participantsData,
   meetingIdx,
 }) => {
-  const [nextUser, setNextUser] = useState('김수환');
+  const [nextUser, setNextUser] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (participantsData) {
+      if (participantsData[meetingIdx + 1]) {
+        setNextUser(participantsData[meetingIdx + 1].name);
+      } else {
+        setNextUser(null);
+      }
+    }
+  }, [participantsData]);
 
   console.log(participantsData);
 
@@ -46,11 +56,14 @@ const VideoHeaderComponent: React.FC<VideoHeaaderProps> = ({
       <div className="flex basis-1/4 justify-end mr-5">
         <TimeLeftComponent min={min} sec={sec} />
       </div>
-      {/* 다음 사람 뜨는 텍스트 부분 */}
-      <div className="flex basis-1/4 justify-center text-white">
-        <p className="text-xl mt-1">NEXT</p>
-        <p className="modal-title">{nextUser}</p>
-      </div>
+      {nextUser ? (
+        <div className="flex basis-1/4 justify-center text-white">
+          <p className="text-xl mt-1">NEXT</p>
+          <p className="modal-title">{nextUser}</p>
+        </div>
+      ) : (
+        <div className="flex basis-1/4"></div>
+      )}
     </div>
   ) : (
     <div className="flex flex-row w-screen my-5">
@@ -60,11 +73,14 @@ const VideoHeaderComponent: React.FC<VideoHeaaderProps> = ({
       <div className="flex basis-1/4 justify-end mr-5">
         <TimeLeftComponent min={min} sec={sec} />
       </div>
-      {/* 다음 사람 뜨는 텍스트 부분 */}
-      <div className="flex basis-1/4 justify-center text-white">
-        <p className="text-xl mt-1">NEXT</p>
-        <p className="modal-title">{nextUser}</p>
-      </div>
+      {nextUser ? (
+        <div className="flex basis-1/4 justify-center text-white">
+          <p className="text-xl mt-1">NEXT</p>
+          <p className="modal-title">{nextUser}</p>
+        </div>
+      ) : (
+        <div className="flex basis-1/4"></div>
+      )}
     </div>
   );
 };
