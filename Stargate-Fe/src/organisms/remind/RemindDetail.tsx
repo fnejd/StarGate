84546dfd5cee08beefd530c0b0780b4a21856ba9
yryn2016 <yryn2016@gmail.com>
2023-08-17 12:57:02 +0900

    feature: 리마인드 페이지 가로 스크롤 구현

--- Stargate-Fe/src/organisms/remind/RemindDetail.tsx
+++ Stargate-Fe/src/organisms/remind/RemindDetail.tsx
@@ -9,22 +9,22 @@ const RemindDetail = ({ meetingMembers }: MeetingMembersProps) => {
     <div className="flex">
       {meetingMembers.map((member) => (
         <div key={member.memberNo}>
-          <h3 className="form-title">{member.name}</h3>
           <div className="flex">
+            <h3 className="ml-10 form-title">{member.name}</h3>
             {member.polaroids.map((polaroid) => (
               <img
-                className="mx-10 w-400 h-200"
+                className="mx-10 w-400 h-400"
                 key={polaroid.no}
                 src={polaroid.imageFileInfo.fileUrl}
                 alt={`폴라로이드 ${polaroid.no}`}
               />
             ))}
+            {member.letter && (
+              <div className="p-4 bg-white rounded-lg w-96 h-96">
+                <p>{member.letter.contents}</p>
+              </div>
+            )}
           </div>
-          {member.letter && (
-            <div className="p-4 bg-white rounded-lg w-96 h-96">
-              <p>{member.letter.contents}</p>
-            </div>
-          )}
         </div>
       ))}
     </div>
